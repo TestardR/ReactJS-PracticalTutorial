8a8c53d28d863c648ed05dafec565b30a642cc5f
Romain <rmtestard@gmail.com>
2018-11-26 20:26:25 +0100

    How to define components : functions & classes

--- src/App.js
+++ src/App.js
@@ -2,24 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Body = () => {
+  return <div>Learning react is great fun !!!</div>;
+};
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Body />
       </div>
     );
   }

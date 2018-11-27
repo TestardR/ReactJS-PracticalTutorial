c613fd6469ecb6bbd5f9d6670adb83b5d6a9dfc9
Romain <rmtestard@gmail.com>
2018-11-27 07:58:43 +0100

    Passing Data to Components as props

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const Body = () => {
-  return <div>Learning react is great fun !!!</div>;
+const Body = props => {
+  return (
+    <div>
+      <div>{props.title}</div>
+      <div>{props.text}</div>
+      <div>{props.myFunc(1, 2)}</div>
+    </div>
+  );
 };
 
 class Header extends Component {
@@ -11,19 +17,33 @@ class Header extends Component {
     return (
       <div>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>{this.props.title}</div>
+        <div>{JSON.stringify(this.props.Obj)}</div>
+        <div>{this.props.Obj.a}</div>
+        <div>{this.props.myArr[0]}</div>
+        <div>{this.props.myFunc(1, 9)}</div>
       </div>
     );
   }
 }
 
 class App extends Component {
+  add(a, b) {
+    return a + b;
+  }
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header
+          title="Hello from App"
+          num={5}
+          Obj={{
+            a: 5,
+            b: 6
+          }}
+          myArr={[1, 2, 3]}
+          myFunc={this.add}
+        />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -32,7 +52,7 @@ class App extends Component {
         >
           Learn React
         </a>
-        <Body />
+        <Body title="Shake your body" text="you are great" myFunc={this.add} />
       </div>
     );
   }

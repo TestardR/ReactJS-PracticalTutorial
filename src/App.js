ddcc97ed476dbba1566096a7ee8990340f0942e9
Romain <rmtestard@gmail.com>
2018-11-27 16:45:03 +0100

    form validation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 
-import Body, { Body2 } from './components/Body';
-
 import './App.css';
-import Header from './components/Header';
-import Counter from './components/Counter';
-import ImageSlider from './components/ImageSlider';
-import MyForm from './components/MyForm';
+import ValdidationForm from './components/ValdidationForm';
 
 class App extends Component {
   state = {
@@ -17,23 +12,9 @@ class App extends Component {
     return a + b;
   }
   render() {
-    // use if statement is you want to totally change the screen
-    // if (!this.state.visible) {
-    //   return <div>display nothing</div>;
-    // }
-
-    // const buttonText = this.state.visible ? 'hide' : 'show';
-    // const slider = this.state.visible ? (
-    //   <ImageSlider />
-    // ) : (
-    //   <div>
-    //     <Counter initialCount={15} />
-    //   </div>
-    // );
-
     return (
       <div className="App">
-        <MyForm />
+        <ValdidationForm />
       </div>
     );
   }
